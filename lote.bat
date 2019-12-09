daf0b5ff2195fd8469235e865b081cf74162ebbc
Alicia <lvilchezo@unprg.edu.pe>
2019-12-08 20:05:12 -0500

    condicional simple 02

--- lote.bat
+++ lote.bat
@@ -2,5 +2,9 @@ python vilchez/simple01.py "zapatillas" 500
 python vilchez/simple01.py "cartera" 200
 python vilchez/simple01.py "sandalias" 400
 
+python vilchez/simple02.py "Alicia" 28 50 10
+python vilchez/simple02.py "Patrick" 28 75 10
+python vilchez/simple02.py "Keyla" 27 65 5
+
 
 
